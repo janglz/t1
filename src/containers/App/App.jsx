9e95c33e1f7f6e2822790c3518847af94a41a1b0
Janglz <wpidish@gmail.com>
2021-08-02 16:55:35 +0300

    GROUP-1 testing commits sync with jira

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,9 +1,11 @@
 import { React } from 'react'
+import logo from "../../styles/img/logo.svg"
 
 function App() {
   return (
     <>
       <h1>App</h1>
+      <img src={logo} width="200px" />
     </>
   )
 }
