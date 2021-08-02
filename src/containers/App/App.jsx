8608368042d145e64078a7c5b9c6fd66584a3321
Janglz <wpidish@gmail.com>
2021-08-02 21:19:57 +0300

    GROUP-10 added json mocks

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,7 +1,11 @@
 import { React } from 'react'
-import logo from "../../styles/img/logo.svg"
+import logo from '../../styles/img/logo.svg'
+import users from '../../db/users.json'
+import organizations from '../../db/organizations.json'
 
 function App() {
+  console.log(users, organizations)
+
   return (
     <>
       <h1>App</h1>
